299dd97908940f3d091a50723d8b098c7f818c47
Takuma-Hiraoka <t-hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2026-03-09 18:06:58 +0900

    fix

--- casadi_vendor-extras.cmake
+++
@@ -1,8 +0,0 @@
-add_library(casadi SHARED IMPORTED)
-
-set_target_properties(casadi PROPERTIES
-  IMPORTED_LOCATION
-    "${casadi_vendor_DIR}/../../../lib/libcasadi.so"
-  INTERFACE_INCLUDE_DIRECTORIES
-    "${casadi_vendor_DIR}/../../../include"
-)
